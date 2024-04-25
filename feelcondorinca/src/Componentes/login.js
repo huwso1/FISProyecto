7cbc29eed2e6ab6820978368468a5bd9d82c2aab
pablo <pablomastername@gmail.com>
2024-04-25 18:01:57 -0500

    El login centrado y la redireccion del boton ingresar

--- feelcondorinca/src/Componentes/login.js
+++ feelcondorinca/src/Componentes/login.js
@@ -1,15 +1,27 @@
 import React from "react";
+import button from "bootstrap/js/src/button"
+import {useNavigate} from "react-router";
 
 function Login() {
+
+    const navigate = useNavigate();
+
     return(
-      <div className="contenedor-login">
-        <h1>Usuario</h1>
-        <input type="text" name="Usuario"/>
-        <h1>Contraseña</h1>
-        <input type="text" name="Contrasena"/>
-        <p></p>
-        <img src={require("./Imagenes/Facilito.png")}/>
-      </div>
+        <figure className="text-center">
+            <blockquote className="blockquote">
+                <div className="contenedor-login">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Usuario</label>
+                    <input type="text" name="Usuario"/>
+                    <h1>Contraseña</h1>
+                    <input type="text" name="Contrasena"/>
+                    <button type="button" className="btn btn-primary" onClick={() => navigate('/NavigateBar')}>Ingresar</button>
+                    <p></p>
+                    <img src={require("./Imagenes/Facilito.png")}/>
+                </div>
+            </blockquote>
+        </figure>
+
     );
 }
+
 export default Login;
