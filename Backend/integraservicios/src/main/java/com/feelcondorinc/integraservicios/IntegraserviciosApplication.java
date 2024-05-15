ffaf2091d77223c7f62774eaad36e0b92e99e424
Holman Alvarado <alvholman@gmail.com>
2024-05-14 22:26:31 -0500

    Add Backend

---
+++ Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/IntegraserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.feelcondorinc.integraservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegraserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegraserviciosApplication.class, args);
+	}
+
+}
---
+++ Backend/integraservicios/src/test/java/com/feelcondorinc/integraservicios/IntegraserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelcondorinc.integraservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
