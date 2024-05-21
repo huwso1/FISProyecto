0dfcf92db5e22d85025a043f47480af6ff4f197b
Holman Alvarado <alvholman@gmail.com>
2024-05-20 23:34:41 -0500

    Remove Services

---
+++ Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/controllers/UsuarioController.java
@@ -0,0 +1,8 @@
+package com.feelcondorinc.integraservicios.controllers;
+
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.web.bind.annotation.RestController;
+
+public class UsuarioController {
+
+}
--- Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/repositories/UsuarioRepository.java
+++ Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/repositories/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.feelcondorinc.integraservicios.repositories;
 
 import com.feelcondorinc.integraservicios.entities.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+
 }
