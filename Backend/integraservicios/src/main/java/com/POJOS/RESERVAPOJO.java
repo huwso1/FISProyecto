9a4c3519609f81395b00f1dbd5fe5127481d83bb
FelipePaez <felipepaezvargas@gmail.com>
2024-05-26 19:31:38 -0500

    Add Reserva

---
+++ Backend/integraservicios/src/main/java/com/POJOS/RESERVAPOJO.java
@@ -0,0 +1,30 @@
+package com.POJOS;
+
+import com.feelcondorinc.integraservicios.entities.models.EstadoReserva;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class RESERVAPOJO {
+
+    private Long idReserva;
+    private int horaInicial;
+    private int minutoInicial;
+    private int horaFinal;
+    private int minutoFinal;
+    private Date fecha;
+    private EstadoReserva estadoReserva;
+    private String observaciones;
+    private Long idRecurso;  
+    private Long idUsuario;  
+
+}
--- Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/services/AfiliadoService.java
+++ Backend/integraservicios/src/main/java/com/feelcondorinc/integraservicios/services/AfiliadoService.java
@@ -7,23 +7,61 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.POJOS.RECURSOPOJO;
+import com.POJOS.RESERVAPOJO;
 import com.feelcondorinc.integraservicios.entities.Horario;
 import com.feelcondorinc.integraservicios.entities.HorarioDisponible;
 import com.feelcondorinc.integraservicios.entities.Recurso;
 import com.feelcondorinc.integraservicios.entities.Reserva;
 import com.feelcondorinc.integraservicios.entities.Unidad;
+import com.feelcondorinc.integraservicios.entities.Usuario;
 import com.feelcondorinc.integraservicios.entities.models.EstadoRecurso;
+import com.feelcondorinc.integraservicios.repositories.RecursoRepository;
+import com.feelcondorinc.integraservicios.repositories.ReservaRepository;
+import com.feelcondorinc.integraservicios.repositories.UsuarioRepository;
+
+
 
 @Service
 public class AfiliadoService {
 
-    public String crearReserva(Reserva reserva) {
-       
+    @Autowired
+    private ReservaRepository reservaRepository;
 
-        return "";
-    }
+    @Autowired
+    private RecursoRepository recursoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public String crearReserva(RESERVAPOJO reservaPOJO) {
+        // Conversión RESERVAPOJO a Reserva
+        Reserva reserva = new Reserva();
+        reserva.setHoraInicial(reservaPOJO.getHoraInicial());
+        reserva.setMinutoInicial(reservaPOJO.getMinutoInicial());
+        reserva.setHoraFinal(reservaPOJO.getHoraFinal());
+        reserva.setMinutoFinal(reservaPOJO.getMinutoFinal());
+        reserva.setFecha(reservaPOJO.getFecha());
+        reserva.setEstadoReserva(reservaPOJO.getEstadoReserva());
+        reserva.setObservaciones(reservaPOJO.getObservaciones());
+
+        // Asociar el recurso a la reserva
+        Optional<Recurso> recursoOpt = recursoRepository.findById(reservaPOJO.getIdRecurso());
+        if (recursoOpt.isPresent()) {
+            reserva.setIdRecurso(recursoOpt.get());
+        } else {
+            return "Recurso no encontrado";
+        }
+
+        // Asociar el usuario a la reserva
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(reservaPOJO.getIdUsuario());
+        if (usuarioOpt.isPresent()) {
+            reserva.setIdUsuario(usuarioOpt.get());
+        } else {
+            return "Usuario no encontrado";
+        }
 
 }
